404caf34cec7819abd96092cd9a7534b082e6de7
dannyweapons <danytazps@gmail.com>
2018-02-01 15:29:01 -0600

    frontend 4ta pagina de los 7 tipos de empresa

--- src/pages/lead/lead.ts
+++ src/pages/lead/lead.ts
@@ -24,4 +24,16 @@ export class LeadPage {
     this.viewCtrl.dismiss();
   }
 
+ /* FbotonOn() {
+    var uno = document.getElementById('tel');
+    if (uno.innerHTML == '26222194 ID cliente 1094') 
+        uno.innerHTML = 'Llamar a Juan Pérez';
+    else uno.innerHTML = '26222194 ID cliente 1094'; 
+  }*/
+
+  FbotonOn() {
+    var uno = document.getElementById('tel');
+    uno.innerHTML = '26222194 ID cliente 1094'; 
+  }
+
 }
