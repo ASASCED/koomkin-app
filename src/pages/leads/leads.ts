d0532778cf281f8ef4205ae7d3a52ef9c4b7dade
dannyweapons <danytazps@gmail.com>
2018-05-17 13:23:14 -0500

    Arreglando cambio de clases de los leads calificacion y clasificacion

--- src/pages/leads/leads.ts
+++ src/pages/leads/leads.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { IonicPage, NavController, NavParams, ModalController } from 'ionic-angular';
 import { RestProvider } from '../../providers/rest/rest';
 import { LeadPage } from '../lead/lead';
+import { HttpClient } from '@angular/common/http';
+
 
 @IonicPage()
 @Component({
@@ -16,12 +18,19 @@ export class LeadsPage implements OnInit {
   leads;
   page = 1;
   user;
-  selectedLike; 
+  selectedLike;
+  public califica;
+  public calificacion;
+  public clave;
+
+  apiUrl = 'http://localhost:3000';
+
 
   constructor(public navCtrl: NavController,
     public navParams: NavParams,
     public provedor: RestProvider,
-    public modalCtrl: ModalController) {
+    public modalCtrl: ModalController,
+    public http: HttpClient) {
   }
 
   ngOnInit() {
@@ -31,22 +40,40 @@ export class LeadsPage implements OnInit {
   public getLeadsReport() {
     this.provedor.getLeadsReport()
       .then(
-      (data) => {
-        this.leads = data;
-        //console.log(this.leads);
-        for (let k in this.leads) {
-          this.leads[k].FECHA = this.leads[k].FECHA.substring(0, 10).replace(/^(\d{4})-(\d{2})-(\d{2})$/g, '$3/$2/$1');
-          this.leads[k].NOMBRE = this.leads[k].NOMBRE.substring(0, 18);
-          this.leads[k].EMPRESA = this.leads[k].EMPRESA.substring(0,35);
-          this.selectedLike =  this.leads[k].calificaLead;
-        }
-
-        //console.log(this.leads);
-      },
-      (error) => {
-        console.log(error);
-      });
+        (data) => {
+          this.leads = data;
+          //console.log(this.leads);
+          for (let k in this.leads) {
+            this.leads[k].FECHA = this.leads[k].FECHA.substring(0, 10).replace(/^(\d{4})-(\d{2})-(\d{2})$/g, '$3/$2/$1');
+            this.leads[k].NOMBRE = this.leads[k].NOMBRE.substring(0, 16);
+            this.leads[k].EMPRESA = this.leads[k].EMPRESA.substring(0, 35);
+            if(this.leads[k].calificaLead == 'True'){
+              this.leads[k].calificaLead = "like";
+            }else if(this.leads[k].calificaLead == 'False'){
+              this.leads[k].calificaLead = "dislike";
+            } else{
+              this.leads[k].calificaLead = "vacio";
+            }
+            if(this.leads[k].clasificaLead == 'En proceso de venta'){
+              this.leads[k].clasificaLead = "P";
+            }else if(this.leads[k].clasificaLead == 'Descartado'){
+              this.leads[k].clasificaLead = "D";
+            }else if(this.leads[k].clasificaLead == 'Vendido'){
+              this.leads[k].clasificaLead = "V";
+            }else if(this.leads[k].clasificaLead == 'Sin Contacto'){
+              this.leads[k].clasificaLead = "S";
+            } else{
+              this.leads[k].calificaLead = "banda";
+            }
+          }
+          console.log(this.leads);
+        },
+        (error) => {
+          console.log(error);
+        });
   }
+
+  
 
   doInfinite(infiniteScroll) {
     console.log('Cargando Leads');
@@ -60,10 +87,44 @@ export class LeadsPage implements OnInit {
     }, 500);
   }
 
+  public changeLike(classification: string, lead) {
+    switch (classification) {
+      case 'L': {
+        this.leads.calificaLead = 'true';
+        this.califica = 'l';
+        lead.calificaLead = 'like';
+        break;
+      }
+      case 'DL': {
+        this.leads.calificaLead = 'false';
+        this.califica = 'd';
+        lead.calificaLead = 'dislike';
+        break;
+      }
+      default:
+        this.califica = null;
+        lead.calificaLead = 'vacio'
+    }
+
+    const body = {
+      classification: this.califica
+    }
+    console.log(lead.clave);
+    this.http.get(this.apiUrl + "/calificaLead/" + lead.clave + '/' + body.classification)
+      .subscribe(data => {
+        this.calificacion = data[0].calificaLead;
+        console.log(this.calificacion);
+      },
+        err => {
+          console.log("Error occured");
+        });
+
+  }
+
   ionViewDidLoad() {
   }
 
-  verLead(lead){
+  verLead(lead) {
     this.navCtrl.push(LeadPage, lead);
   }
 
