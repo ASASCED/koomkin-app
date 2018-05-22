8b98bb1ff8c11d7681ad9270b986ccc847126fd8
Dannyweapons <danytazps@gmail.com>
2018-05-22 14:13:23 -0500

    deploy

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+var cors = require('cors')
 const db = require('./db.js');
 const _ = require('underscore');
 
@@ -17,6 +18,8 @@ if ('development' == app.get('env')) {
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 //app.use(logger('dev'));
 //app.use(cookieParser());
+app.use(cors())
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
