a1a86beb7599da4a2829a7c58982dd5f53dbf8f4
Dannyweapons <danytazps@gmail.com>
2019-02-21 19:05:40 -0600

    amazon

--- conf/conf.js
+++ conf/conf.js
@@ -1,20 +1,37 @@
-let conf = {
+module.exports.config = {
+
+  /*** Datos de acceso API sandbox devfactura.com ***/
+  // API_KEY_P: 'JDJ5JDEwJHhNVGdnZVltWjJHQUQzLkIuWGIzbnVPeUF2S0J4eEhRcXg5c2pIVEtMWkJJMXdudVNQNVJ1',
+  // SCT_KEY_P: 'JDJ5JDEwJHgzTHRGdFdiMnR1Zy5XOWllek11U09YQjlOWDY0Uk5NQnFoQ3RaMXRySXdmNVZCZEYvYmIu',
+  // URL_FACTURA_P: 'http://devfactura.in/api',
+
+  /*** Datos de acceso API factura.com ***/
+  API_KEY: 'JDJ5JDEwJG55U0Vsd2VGZVVnRk12T3V6ZXZoS3VzOWhVNXNwV1NXc1h4bTBVOWd4bUY1dU12NXo4bDFX',
+  SCT_KEY: 'JDJ5JDEwJEVIdVloNE1DUFZIZmlmZnpkOU1xRWViUUxHRXFVYVc2NEhvYS9ZUkpPenM2NkZiUmZYWDZT',
+  URL_FACTURA: 'https://factura.com/api',
+
+  /*** API Mandrill mailing ***/
+  MANDRILL_KEY: 'ALev50nS5NboejBdJhmuiQ',
+
+
   userName: 'USERKOOMKIN',
-  password: 'Ag0K00M',
-  //password: 'U$3rk00mk1n',
-  //server: '187.162.76.74',
-  //server: '192.168.5.70',
-  //server:'187.188.105.90',
-  server: '189.205.233.70',
+  /*** Variables de desarrollo ***/
+  // HOST: 6842,
+  // password: 'Ag0K00M',
+  // server: '187.162.208.218',
+  // options: {
+  //   port: 1439,
+  //   database: 'aaa'
+  // }
 
-  options: {
-      port: 1439,
-      //port:1433,
-      //database: 'AGO_KOOMKIN'
-      database: 'aaa'
-  },
+  /*** Variables de producción ***/
+   HOST: 5545,
+   //HOST: 6843,
+   password: 'U$3rk00mk1n',
+   server: '189.205.233.70',
+   options:{
+     port:1433,
+     database: 'AGO_KOOMKIN'
+   }
 
-  FB_API_VERSION: "2.10"
-};
-
-module.exports.conf;
+}
