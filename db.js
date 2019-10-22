401c0f51baf771f397059948c8621a3166039739
Dannyweapons <danytazps@gmail.com>
2019-10-21 19:35:44 -0500

    cambio server

--- db.js
+++ db.js
@@ -1725,7 +1725,7 @@ db.executeRegistrarClientesNuevos = function(
     publicidadTracicional,
     publicidadDigital
   ) {
-    const requestStr = `Update tbl_brief set ClientesNuevos = ${clientesNuevos}, tipoPublicidad = '${tipoPublicidad}',  publicidadTracicional = '${publicidadTracicional}', publicidadDigital = '${publicidadDigital}' where IDUSUARIO = ${idUsuario}`;
+    //const requestStr = `Update tbl_brief set ClientesNuevos = ${clientesNuevos}, tipoPublicidad = '${tipoPublicidad}',  publicidadTracicional = '${publicidadTracicional}', publicidadDigital = '${publicidadDigital}' where IDUSUARIO = ${idUsuario}`;
   
     const requestStr = `Update tbl_brief set ClientesNuevos = ${clientesNuevos} where IDUSUARIO = ${idUsuario}`
     console.log(requestStr);
