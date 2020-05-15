e6e5256f30f64792fff1c25ef8707f413ef17bb0
Jesus Gutierrez <jesus.gutierrez.cs@gmail.com>
2020-05-14 20:40:01 -0500

    Fix height for modal upsells

--- src/pages/inicio/inicio.ts
+++ src/pages/inicio/inicio.ts
@@ -192,7 +192,7 @@ export class InicioPage implements OnInit {
       {
         data: offerData,
       },
-      { enableBackdropDismiss: true, cssClass: "Modal-comentario" }
+      { enableBackdropDismiss: true, cssClass: "Modal-upsells" }
     );
     myModal.present();
     myModal.onDidDismiss(() => {});
